22ee66bc6e770f590b1e2f80cd945e8df541e386
Velmurugan Periasamy <vel@apache.org>
2015-04-24 16:40:39 -0400

    BUG-34504: Remove condition to copy DB driver jar file

--- agents-common/scripts/enable-agent.sh
+++ agents-common/scripts/enable-agent.sh
@@ -402,8 +402,8 @@ fi
 if [ "${action}" = "enable" ]
 then
 
-	if [ -d "${PROJ_LIB_DIR}" ]
-	then
+	#if [ -d "${PROJ_LIB_DIR}" ]
+	#then
 		dt=`date '+%Y%m%d%H%M%S'`
 		dbJar=`grep '^SQL_CONNECTOR_JAR' ${INSTALL_ARGS} | awk -F= '{ print $2 }'`
 		for f in ${PROJ_LIB_DIR}/*.jar ${dbJar}
@@ -422,7 +422,7 @@ then
 				fi
 			fi
 		done
-	fi
+	#fi
 
 	#
 	# Encrypt the password and keep it secure in Credential Provider API
