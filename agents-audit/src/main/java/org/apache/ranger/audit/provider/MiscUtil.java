31763c7cf2f8a5b0eb42ef6ac4c972285ff0eb5c
rmani <rmani@hortonworks.com>
2016-10-04 12:36:01 -0700

    HOTFIX-687 BUG-66702 RANGER-1097: Ranger Plugin should not fail to download policy when UGI ticket expires
    Change-Id: I4be9d9376dc8ac9e73eea75421014621425ccf46

--- agents-audit/src/main/java/org/apache/ranger/audit/provider/MiscUtil.java
+++ agents-audit/src/main/java/org/apache/ranger/audit/provider/MiscUtil.java
@@ -480,14 +480,32 @@ public class MiscUtil {
 	}
 
 	public static UserGroupInformation getUGILoginUser() {
-		if (ugiLoginUser == null) {
+		UserGroupInformation ret = ugiLoginUser;
+
+		if (ret == null) {
 			try {
-				ugiLoginUser = UserGroupInformation.getLoginUser();
+				// Do not cache ugiLoginUser if it is not explicitly set with
+				// setUGILoginUser.
+				// It appears that the user represented by
+				// the returned object is periodically logged out and logged back
+				// in when the token is scheduled to expire. So it is better
+				// to get the user object every time from UserGroupInformation class and
+				// not cache it
+				ret = getLoginUser();
 			} catch (IOException e) {
 				logger.error("Error getting UGI.", e);
 			}
 		}
-		return ugiLoginUser;
+
+		if(ret != null) {
+			try {
+			ret.checkTGTAndReloginFromKeytab();
+			} catch(IOException excp) {
+				// ignore
+			}
+		}
+
+		return ret;
 	}
 
 	public static Subject getSubjectLoginUser() {
@@ -770,6 +788,10 @@ public class MiscUtil {
 					AppConfigurationEntry.LoginModuleControlFlag.REQUIRED,
 					options), };
 		}
+	}
+
+	public static UserGroupInformation getLoginUser() throws IOException {
+		return UserGroupInformation.getLoginUser();
 	}
 
 	private static  void initLocalHost() {
