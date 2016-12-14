0a2b480b50a1157b24b2b6bee3206ec0ba4018c2
pradeep agrawal <pradeep.agrawal@freestoneinfotech.com>
2016-12-14 11:08:42 +0530

    RANGER-1249:Ranger admin service should run with 'UTC' default timezone
    Change-Id: I1b9a3578d063779d70f516ee97d78501495365d3

--- embeddedwebserver/scripts/ranger-admin-services.sh
+++ embeddedwebserver/scripts/ranger-admin-services.sh
@@ -31,7 +31,7 @@ RANGER_JAAS_LIB_DIR="${XAPOLICYMGR_EWS_DIR}/ranger_jaas"
 RANGER_JAAS_CONF_DIR="${XAPOLICYMGR_EWS_DIR}/webapp/WEB-INF/classes/conf/ranger_jaas"
 pidf=/var/run/ranger/rangeradmin.pid
 JAVA_OPTS=" ${JAVA_OPTS} -XX:MaxPermSize=256m -Xmx1024m -Xms1024m "
-
+if [[ ${JAVA_OPTS} != *"-Duser.timezone"* ]] ;then  export JAVA_OPTS=" ${JAVA_OPTS} -Duser.timezone=UTC" ;fi
 if [ -f ${XAPOLICYMGR_DIR}/ews/webapp/WEB-INF/classes/conf/java_home.sh ]; then
         . ${XAPOLICYMGR_DIR}/ews/webapp/WEB-INF/classes/conf/java_home.sh
 fi
