9aa2e258ec7f2b542656440ddfa351dd5bac97e3
Nikhil P <npurbhe@hortonworks.com>
2018-11-02 19:32:26 +0530

    BUG-113510 : RANGER-2273 : Allow service admin and delegated admin user to view list of users and groups though they have 'USER' role
    Change-Id: Ib98a0fdd2933c30a2832255f8732fd0ef2015efe

--- security-admin/src/main/webapp/scripts/views/reports/UserAccessLayout.js
+++ security-admin/src/main/webapp/scripts/views/reports/UserAccessLayout.js
@@ -631,7 +631,7 @@ define(function(require) {'use strict';
 					callback(data);
 				},
 				ajax: { 
-					url: "service/xusers/groups",
+					url: "service/xusers/lookup/groups",
 					dataType: 'json',
 					data: function (term, page) {
 						return {name : term};
@@ -640,8 +640,8 @@ define(function(require) {'use strict';
 						var results = [],selectedVals = [];
 						if(!_.isEmpty(that.ui.userGroup.val()))
 							selectedVals = that.ui.userGroup.val().split(',');
-						if(data.resultSize != "0"){
-							results = data.vXGroups.map(function(m, i){	return {id : m.name, text: _.escape(m.name) };	});
+						if(data.totalCount != "0"){
+							results = data.vXStrings.map(function(m){	return {id : m.value, text: _.escape(m.value) };	});
 							if(!_.isEmpty(selectedVals))
 								results = XAUtil.filterResultByIds(results, selectedVals);
 							return {results : results};
@@ -685,7 +685,7 @@ define(function(require) {'use strict';
 					callback(data);
 				},
 				ajax: { 
-					url: "service/xusers/users",
+					url: "service/xusers/lookup/users",
 					dataType: 'json',
 					data: function (term, page) {
 						return {name : term};
@@ -694,8 +694,8 @@ define(function(require) {'use strict';
 						var results = [],selectedVals=[];
 						if(!_.isEmpty(that.ui.userName.select2('val')))
 							selectedVals = that.ui.userName.select2('val');
-						if(data.resultSize != "0"){
-							results = data.vXUsers.map(function(m, i){	return {id : m.name, text: _.escape(m.name) };	});
+						if(data.totalCount != "0"){
+							results = data.vXStrings.map(function(m){	return {id : m.value, text: _.escape(m.value) };	});
 							if(!_.isEmpty(selectedVals))
 								results = XAUtil.filterResultByIds(results, selectedVals);
 							return {results : results};
