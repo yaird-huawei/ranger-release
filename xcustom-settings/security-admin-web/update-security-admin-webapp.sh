8cdb9a1de5915a2f1191dd6b105501f18b698c30
Yair Diaz <y00495068@china.huawei.com>
2019-07-26 11:15:41 +0200

    sh file 2

--- xcustom-settings/security-admin-web/update-security-admin-webapp.sh
+++ xcustom-settings/security-admin-web/update-security-admin-webapp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #find -L /usr/hdp -name "webapp"
-
-docker cp /c/Users/y00495068/dev/code/yaird/ranger-release/security-admin/src/main/webapp sandbox-hdp:/usr/hdp/current/ranger-admin/ews/
+cp -r /c/Users/y00495068/dev/code/yaird/ranger-release/security-admin/src/main/webapp /c/Users/y00495068/dev/code/yaird/ranger-release/xcustom-settings/security-admin-web/webapp
+docker cp webapp sandbox-hdp:/usr/hdp/current/ranger-admin/ews/
+rm -rf webapp
 
 echo done!
 
